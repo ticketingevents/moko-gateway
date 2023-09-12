344fd39ee042e58be7756f45e0df0fafe960608e
dadlian <sveninem@gmail.com>
2023-09-12 23:25:15 +0100

    Improved echo task

--- lib/tasks/system/echo.lua
+++ lib/tasks/system/echo.lua
@@ -5,7 +5,18 @@ local Task = require "moko.task".Task
 Echo = Task:new()
 
 function Echo:execute(input)
-	local output = input
+  local count = 0
+  for _ in pairs(input) do count = count + 1 end
+
+  local output = input
+
+  -- If there is only one input entry, echo its contents instead
+  if count == 1 then
+    for k in pairs(input) do
+      output = input[k]
+    end
+  end
+
 	return self:ok(output)
 end
 
